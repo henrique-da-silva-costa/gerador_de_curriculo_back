a152579f4077372692dffabec20573d21b964a54
Henrique <81424965+henrique-da-silva-costa@users.noreply.github.com>
2025-03-03 11:22:22 -0300

    ajustes para testar

--- app/controllers/ValidacaoCurriculo.php
+++ app/controllers/ValidacaoCurriculo.php
@@ -28,23 +28,27 @@ class ValidacaoCurriculo
         try {
             $dataNascimento = isset($dados["data_nascimento"]) ? Carbon::createFromFormat("Y-m-d", $dados["data_nascimento"]) : NULL;
         } catch (\Throwable $th) {
-            return print_r(json_encode(["erro" => TRUE, "msg" => "data invalida", "campo" => "data_nascimento"]));
+            return print_r(json_encode(["erro" => TRUE, "msg" => "Campo obrigatório/data invalida", "campo" => "data_nascimento"]));
         }
 
         try {
             $dataInicio = isset($dados["data_inicio"]) ? Carbon::createFromFormat("Y-m-d", $dados["data_inicio"]) : NULL;
         } catch (\Throwable $th) {
-            return print_r(json_encode(["erro" => TRUE, "msg" => "data invalida", "campo" => "data_inicio"]));
+            return print_r(json_encode(["erro" => TRUE, "msg" => "Campo obrigatório/data invalida", "campo" => "data_inicio"]));
         }
 
         try {
             $dataFim = isset($dados["data_fim"]) ? Carbon::createFromFormat("Y-m-d", $dados["data_fim"]) : NULL;
         } catch (\Throwable $th) {
-            return print_r(json_encode(["erro" => TRUE, "msg" => "data invalida", "campo" => "data_fim"]));
+            return print_r(json_encode(["erro" => TRUE, "msg" => "Campo obrigatório/data invalida", "campo" => "data_fim"]));
         }
 
         foreach ($dados as $index => $dado) {
-            if (strlen($dado) < 1 && $index != "data_fim") {
+            // if (strlen($dado) < 1 && $index == "sexo") {
+            //     return print_r(json_encode(["erro" => TRUE, "msg" => "campo obrigatorio", "campo" => "sexo"]));
+            // }
+
+            if (strlen($dado) < 1 && $index != "data_fim" && $index != "img") {
                 return print_r(json_encode(["erro" => TRUE]));
             }
 
@@ -60,17 +64,23 @@ class ValidacaoCurriculo
 
             if ($dataInicio) {
                 if ($dataInicio->gt($dataFim)) {
-                    return print_r(json_encode(["erro" => TRUE, "msg" => "A data de inicio não pode ser maior que a data e fdinal", "campo" => "data_inicio"]));
+                    return print_r(json_encode(["erro" => TRUE, "msg" => "A data de inicio não pode ser maior que a data final", "campo" => "data_inicio"]));
                 }
 
-                if (!$dataInicio->eq($dataNascimento->copy()->addYears(14))) {
-                    return print_r(json_encode(["erro" => TRUE, "msg" => "A data de inicio não corresponde com a dta de nascimento", "campo" => "data_inicio"]));
+                $diferencaDeAnos = $dataNascimento->diffInYears($dataInicio);
+
+                if ($diferencaDeAnos < 14) {
+                    return print_r(json_encode(["erro" => TRUE, "msg" => "A data de inicio não corresponde com a data de nascimento", "campo" => "data_inicio"]));
                 }
             }
 
-            // if ($dataFim < $dataInicio->addDays(90)) {
-            //     return print_r(json_encode(["erro" => TRUE, "msg" => "A data de inicio não pode ser menor que 3 meses", "campo" => "data_inicio"]));
-            // }
+            if ($dataInicio && $dataFim) {
+                $diferencaDeAnos = $dataInicio->diffInMonths($dataFim);
+
+                if ($diferencaDeAnos < 3) {
+                    return print_r(json_encode(["erro" => TRUE, "msg" => "A data de inicio não pode ser menor que 3 meses", "campo" => "data_inicio"]));
+                }
+            }
 
             if (strlen($dado) > 255 && $index != "img" && $index != "descricao") {
                 return print_r(json_encode(["erro" => TRUE, "msg" => "limite maximo de caracteres 255"]));
