4553e7cc7307452cdf7ccb218ea7651b76be3d03
Henrique <81424965+henrique-da-silva-costa@users.noreply.github.com>
2025-03-04 09:29:58 -0300

    ajustando verificação tipo de email

--- app/controllers/Validacao.php
+++ app/controllers/Validacao.php
@@ -4,8 +4,6 @@ namespace app\controllers;
 
 class Validacao
 {
-    const INPUTS = ["nome", "email", "senha", "novaSenha", "comfirmaSenha"];
-
     public static function validar($dados)
     {
         $regex = '/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/';
@@ -14,24 +12,12 @@ class Validacao
             return print_r(json_encode(["erro" => TRUE, "msg" => "Tipo de dado invalido"]));
         }
 
-        // if (!isset($dados["nome"])) {
-        //     return print_r(json_encode(["erro" => TRUE, "msg" => "campo obrigatório"]));
-        // }
-
-        // if (!isset($dados["email"])) {
-        //     return print_r(json_encode(["erro" => TRUE, "msg" => "campo obrigatório"]));
-        // }
-
-        // if (!isset($dados["senha"])) {
-        //     return print_r(json_encode(["erro" => TRUE, "msg" => "campo obrigatório"]));
-        // }
-
         foreach ($dados as $index => $dado) {
             if (strlen($dado) < 1 && $index != "img") {
                 return print_r(json_encode(["erro" => TRUE, "msg" => "campo obrigatório", "campo" => $index]));
             }
 
-            if ($index == "email" && !preg_match($regex, $dado)) {
+            if ($index == "emailVerificar" && !preg_match($regex, $dado)) {
                 return print_r(json_encode(["erro" => TRUE, "msg" => "tipo de email inválido", "campo" => $index]));
             }
 
